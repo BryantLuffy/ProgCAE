a85cc90a3b011ef02c727531e90157506db07e2b
BryantLuffy <103727213+BryantLuffy@users.noreply.github.com>
2023-03-13 18:10:20 +0800

    Update Prog_CAE.py

--- Prog_CAE.py
+++ Prog_CAE.py
@@ -69,7 +69,7 @@ flatten = pd.concat([pd.DataFrame(CNV_feature),
                      pd.DataFrame(Meth_feature)], axis=1)
 SURVIVE_SELECT = survive_select(survive, flatten, 0.01)
 # Save the dataframe to a file
-flatten.to_csv('ProgCAE_features.csv')
+SURVIVE_SELECT.to_csv('ProgCAE_features.csv')
 
 # Perform clustering and survival analysis
 
